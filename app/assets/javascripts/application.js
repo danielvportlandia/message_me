f0f537539d3bd011f0e69c51796c79cc2813c5e2
daniel shelton <danielshelton90@gmail.com>
2021-05-27 15:39:26 -0700

    submits message on Enter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,10 +22,20 @@ scrollBottom = function() {
   if (messages.length) messages.scrollTop(messages[0].scrollHeight);
 };
 
+submitMessage = function() {
+  $('#message_body').on('keydown', function(e) {
+    if (e.keyCode === 13) {
+      $('button').click();
+      e.target.value = '';
+    }
+  });
+};
+
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  submitMessage();
   scrollBottom();
 });
