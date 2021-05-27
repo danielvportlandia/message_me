917bd7a10414cca9bf9847799da68fb51e6cf28f
daniel shelton <danielshelton90@gmail.com>
2021-05-26 21:45:37 -0700

    created User model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Dan", password: "password")
+User.create(username: "Virgil", password: "password")
+User.create(username: "Beckham", password: "password")
+User.create(username: "Ezekiel", password: "password")
+User.create(username: "Hugo", password: "password")
+User.create(username: "Dee", password: "password")
+User.create(username: "Sky", password: "password")
+User.create(username: "Jr.", password: "password")
