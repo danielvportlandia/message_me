fc1c104d6235dde57d0bbf85887f1038cd7acff2
daniel shelton <danielshelton90@gmail.com>
2021-05-27 09:44:23 -0700

    built out users resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
