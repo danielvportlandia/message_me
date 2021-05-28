78e84ec079704cbfeaf4479903489c8c3b63b668
daniel shelton <danielshelton90@gmail.com>
2021-05-27 18:13:26 -0700

    ActionCable production config changes - 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   config.action_cable.mount_path = nil
-  config.action_cable.url = 'ws://actioncable-gorails.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = ['http://actioncable-gorails.herokuapp.com']
+  config.action_cable.url = 'wss://message-me-0624.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://message-me-0624.herokuapp.com', /http:\/\/message-me-0624.herokuapp.com.*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
